5f3023cc8f3501c50ddabf23e3357b7ad6830b17
Rizky Firmansyah <55770737+rizkyfirmansyah14@users.noreply.github.com>
2020-04-08 12:05:43 +0700

    menambahkan no telp & email serta menghapus alamat

--- aboutme.php
+++ aboutme.php
@@ -2,7 +2,8 @@
 echo "Nama Lengkap : Rizky Firmansyah<br>";
 echo "Kelas        : XI RPL 1<br>";
 echo "Asal Sekolah : SMK Taruna Bhakti<br>";
-echo "Alamat       : Cisalak Depok"
+echo "No Telp      : 081294321405<br>";
+echo "Email        : rizkyfirmansyah140@gmail.com";
 
 
 ?>
